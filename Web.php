27d9b9f25e8b0904d59572dd31b787629141ef27
nadiii04 <91132797+nadiii04@users.noreply.github.com>
2021-12-25 01:55:38 +0700

    Add files via upload

---
+++ Web.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Web extends CI_Controller {
+
+	function _construct(){
+		parent::_construct();
+	}
+
+	public function index(){
+		$data['judul'] = "Halaman Depan"
+		$this->load->view('v_header',$data)
+		$this->load->view('v_index',$data)
+		$this->load->view('v_footer',$data)
+	}
