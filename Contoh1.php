4a64b016dbe977678917b79b5168485a5146e5aa
nadiii04 <91132797+nadiii04@users.noreply.github.com>
2021-12-24 13:06:06 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+
+class Contoh1 extends CI_Controller 
+{
+
+	public function index()
+	{
+		echo "<h3>Perkenalkan</h3>";
+		echo "Nama saya Prinadi Kusumajati 
+			  <br>Saya tinggal di daerah Cikarang 
+			  <br>Olahraga yang saya sukai adalah Bulutangkis";
+	}
+}
---
+++ view-latihan1.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Latihan1</title>
+</head>
+<body>
+	Hallo Kawan.. Yuk kita belajar web progamming..!!<br>
+	Nilai 1 = <?= $nilai1; ?><br>
+	Nilai 2 = <?= $nilai2; ?><br>
+	Ini hasil dari pemodelan dengan method penjumlahan yaitu 
+	<?= $nilai1 . $nilai2 . " + " . $hasil; ?>
+</body>
+</html>
