e978f479c43d8f64f156fdd0964299a068466154
nadiii04 <91132797+nadiii04@users.noreply.github.com>
2021-12-27 20:26:28 +0700

    Add files via upload

---
+++ v_index.php
@@ -0,0 +1,16 @@
+<section>
+	<h1><?php echo $judul ?></h1>
+	<p align="justify">Pada pengertian CodeIgniter diatas tadi dijelaskan bahwa codeigniter menggunakan metode MVC. Apa itu MVC? Kita juga harus tahu apa itu MVC sebelum masuk dan lebih jauh dalam belajar codeigniter
+	</p>
+	<p>
+		MVC adalah teknik atau konsep yang memisahkan komponen utama menjadi tiga komponen yaitu model, view, dan controller
+	</p>
+	<ol type="a">
+		<li>Model</li>
+		<p align="justify">Model adala kelas yang mempresentasikan atau memodelkan tipe data yang akan digunakan oleh aplikasi. Model juga dapa didefinisikan sebagai bagian penanganan yang berhubungan dengan pengolahan atau memenipulasi database</p>
+		<p align="justify">Sebagai catatan, semua model harus disimpan di dalam folder application\models</p>
+		<p align="justify">View merupakan bagian yang menangani halaman user interface atau halaman yang muncul pada user(pada browser)</p>
+		<p align="justify">Controller merupakan kumpulan intruksi aksi yang menghubungkan model dan view</p>
+		<p align="justify">Dari penjelasan tentang model, view dan controller di atas dapat disimpulkan bahwa controller sebagai penghubung view dan model</p>
+	</ol>
+</section>
